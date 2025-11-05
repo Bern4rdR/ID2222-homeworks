cf20e5101f0139af7ea0624b3af1e5e6990787d0
bernardr <bernardr@kth.se>
2025-11-05 15:43:40 +0100

    clean up unused code

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,15 +1,15 @@
 import numpy as np
-from functools import reduce
 import hashlib
-from math import pow, ceil
+from math import ceil
 import time
 from get_data import get_drug_data
+import pandas as pd
 
 shingle_size = 5
 
 def generate_hashes(numHashes: int):
     funcs = []
-    prime = 2147483647 # larget 32 bit prime
+    prime = 2147483647 # largest 32 bit prime
     rng = np.random.default_rng(1234)
     al = rng.integers(1, np.iinfo(np.uint32).max, size=numHashes, dtype=np.uint32).tolist()
     bl = rng.integers(0, np.iinfo(np.uint32).max, size=numHashes, dtype=np.uint32).tolist()
@@ -23,8 +23,6 @@ def generate_hashes(numHashes: int):
     return funcs
 
 def shingle(k, doc):
-    # currently case-sensitive
-    # each hash is 32 bits
     return set([hash(doc[i : i + k]) for i in range(len(doc) - k + 1)])
 
 def shingle_md5(k, doc):
@@ -57,13 +55,14 @@ def minhash(times, shingles):
 
     return np.array(sig_matrix)
 
-# should be less accurate I think??? 
-def fastminhash(times, shingles):
+
+# less accurate
+def fast_minhash(times, shingles):
     all_sh = np.unique(np.concatenate(shingles))
     rows = [np.isin(all_sh, sh_arr) for sh_arr in shingles]
     # row is document, column is shingle
     shingle_matrix = np.array(rows)
-    k = times #k = min(times, shingle_matrix.shape[1]) # fix later
+    k = min(times, shingle_matrix.shape[1])
     hashes = generate_hashes(k)
     # hashes for shingle n are columns, shingles are rows
     hash_vals = np.array([[hashes[i](all_sh[r]) for i in range(k)] for r in range(all_sh.shape[0])])
@@ -74,24 +73,7 @@ def fastminhash(times, shingles):
                 for i in range(signature_matrix.shape[1]):
                         signature_matrix[r, i] = min(signature_matrix[r,i], hash_vals[c, i])
     return signature_matrix
-        
-# this one hashes index instead of shingle... (as per the docs)
-def fastminhashindex(times, shingles):
-    all_sh = np.unique(np.concatenate(shingles))
-    rows = [np.isin(all_sh, sh_arr) for sh_arr in shingles]
-    # row is document, column is shingle
-    shingle_matrix = np.array(rows)
-    k = times #k = min(times, shingle_matrix.shape[1]) # fix later
-    hashes = generate_hashes(k)
-    # hashes for shingle n are columns, shingles are rows
-    hash_vals = np.array([[hashes[i](r) for i in range(k)] for r in range(all_sh.shape[0])])
-    signature_matrix = np.full((shingle_matrix.shape[0], k), np.inf)
-    for r in range(shingle_matrix.shape[0]):
-        for c in range(shingle_matrix.shape[1]):
-            if shingle_matrix[r, c]:
-                for i in range(signature_matrix.shape[1]):
-                        signature_matrix[r, i] = min(signature_matrix[r,i], hash_vals[c, i])
-    return signature_matrix
+
 
 def compare(k, doc1, doc2, perms=100):
     sh1 = shingle_md5(k, doc1)
@@ -99,7 +81,6 @@ def compare(k, doc1, doc2, perms=100):
 
     mh = minhash(perms, [list(sh1), list(sh2)])
     same = sum([1 for i in range(len(mh)) if mh[i][0] == mh[i][1]])
-
     return float(same) / len(mh)
 
 def jaccard_ndarray(nd1, nd2):
@@ -107,39 +88,35 @@ def jaccard_ndarray(nd1, nd2):
     match_rows = sum([1 for i in range(nd1.shape[0]) if nd1[i] == nd2[i]])
     return match_rows/nd1.shape[0]
    
-def minhashmany(doc_list, permutations):
+
+def minhash_many(doc_list, permutations):
     shingle_list = [list(shingle(shingle_size, doc)) for doc in doc_list]
     start = time.perf_counter()
     signature = minhash(permutations, shingle_list)
     end = time.perf_counter()
-    print(f"Min Hash Time ms: {(end - start)} on {len(doc_list)} documents")
-
-
-def fastminmany(doc_list, permutations):
-    shingle_list = [list(shingle_md5(shingle_size, doc)) for doc in doc_list]
-    start = time.perf_counter()
-    signature = fastminhash(permutations, shingle_list)
-    end = time.perf_counter()
-    print(f"Fast Min Hash Time ms: {(end - start)} on {len(doc_list)} documents")
+    print(f"Min Hash Time: {(end - start):.2f}ms on {len(doc_list)} documents")
     return signature
 
-def fastminindexmany(doc_list, permutations):
+
+def fast_min_many(doc_list, permutations):
     shingle_list = [list(shingle_md5(shingle_size, doc)) for doc in doc_list]
     start = time.perf_counter()
-    signature = fastminhashindex(permutations, shingle_list)
+    signature = fast_minhash(permutations, shingle_list)
     end = time.perf_counter()
-    print(f"Fast Min Hash Index Time ms: {(end - start)} on {len(doc_list)} documents")
+    print(f"Fast Min Hash Time: {(end - start):.2f}ms on {len(doc_list)} documents")
+    return signature
+
 
 def hash_many_basic(many):
     a = 762
     b = 984747
-    prime = 2147483647 # larget 32 bit prime
+    prime = 2147483647 # largest 32 bit prime
     return sum(a*x + b for x in many) % prime
 
+
 def find_pairs(doc_list, permutations, min_thresholds): 
-    sig_matrix = fastminmany(doc_list, permutations)
+    sig_matrix = fast_min_many(doc_list, permutations)
     sims = []
-    start = time.perf_counter()
     for threshold in min_thresholds:
         count = 0
         sim_pair = None
@@ -148,36 +125,22 @@ def find_pairs(doc_list, permutations, min_thresholds):
                 if jaccard_ndarray(sig_matrix[i], sig_matrix[j]) > threshold:
                     if count == 0:
                         sim_pair = (doc_list[i], doc_list[j])
-                        count += 1
-                    else: count += 1
+                    count += 1
         if count > 0:
             sims.append((count, sim_pair[0], sim_pair[1]))
         else: sims.append((count, "", ""))
-    run_time = time.perf_counter() - start
-    for sim in sims:
-        print(f"Similar Count: {sim[0]}\n Doc 1: {sim[1]} \n Doc 2: {sim[2]}")
-    print(f"Similar Find time (s): {run_time}")
     return sims
 
 def find_pairs_lsh(doc_list, permutations, min_thresholds):
-    sig_matrix = fastminmany(doc_list, permutations)
-    sims = []
-    start = time.perf_counter()
+    sig_matrix = fast_min_many(doc_list, permutations)
     pairs = []
     for threshold in min_thresholds:
         pairs.append(lsh_pairs(sig_matrix, threshold))
-    run_time = time.perf_counter() - start
-    for i, threshold in enumerate(min_thresholds):
-        print(f"\n\n----- At threshold: {threshold} found {len(pairs[i])} pairs -----")
-        if len(pairs[i]):
-            doc1_ind = pairs[i][0][0]
-            doc2_ind = pairs[i][0][1]
-            print(f"Example Pair\nDoc 1:\n{doc_list[doc1_ind]}\n\nDoc 2:\n{doc_list[doc2_ind]}")
     return pairs
 
 def lsh_pairs(sig_matrix, threshold):
     band_size = threshold_to_band_size(sig_matrix.shape[1], threshold)
-    print(f"Band Size {band_size} at threshold {threshold} on {sig_matrix.shape[1]} unique shingles.")
+    # print(f"Band Size {band_size} at threshold {threshold} on {sig_matrix.shape[1]} unique shingles.")
     buckets = {} # dict to store the values the bands hash to
     # rows are docs, columns are shingles
     for r in range(sig_matrix.shape[0]):
@@ -212,24 +175,17 @@ Test and benchmarking below this point
 """
 
 def benchmark_minhash(doc_list, permutations):
-    minhashmany(doc_list, permutations)
-    fastminmany(doc_list, permutations)
-    # fastminindexmany(doc_list, permutations)
+    minhash_many(doc_list, permutations)
+    fast_min_many(doc_list, permutations)
 
 def basic_test():
     doc1 = "Hello, World!"
     doc2 = "hello, world!"
-    # br, ser, cr = get_drug_data()
-    # doc1 = br[0]
-    # doc2 = br[1]
-    sh1 = shingle(3, doc1)
-    sh2 = shingle(3, doc2)
-    sh1_m5 = shingle_md5(3, doc1)
-    sh2_m5 = shingle_md5(3, doc2)
-    signature = fastminhash(100, [list(sh1_m5), list(sh2_m5)]) 
+    sh1 = shingle_md5(3, doc1)
+    sh2 = shingle_md5(3, doc2)
+    signature = fast_minhash(100, [list(sh1), list(sh2)]) 
     print(f"Jaccard Fast Min Hash: {jaccard_ndarray(signature[0], signature[1])}")
 
-    # print(f"Minhash:\n{minhash(10, [list(sh1), list(sh2)])}")
     print(f"Sim. estimate: {compare(3, doc1, doc2)}")
     print(f"Jaccard: {jaccard(sh1, sh2)}")
 
@@ -238,41 +194,41 @@ def real_doc_test():
     doc_lists = get_drug_data()
     brc = doc_lists[0][0:2]
     shingles_fmh = [list(shingle_md5(shingle_size, doc)) for doc in brc[:2]]
-    sig_fmh = fastminhash(perms, shingles_fmh)
-    # sig_fmhi = fastminhashindex(perms, shingles_fmh) # always shoots high and isn't faster
+    sig_fmh = fast_minhash(perms, shingles_fmh)
     print(f"Min Hash sim esitmate: {compare(shingle_size, brc[0], brc[1], perms)}")
     print(f"Jaccard Fast Min Hash: {jaccard_ndarray(sig_fmh[0], sig_fmh[1])}")
-    # print(f"Jaccard Fast Min Index Hash: {jaccard_ndarray(sig_fmhi[0], sig_fmhi[1])}")
     print(f"Jaccard: {jaccard(set(shingles_fmh[0]), set(shingles_fmh[1]))}")
 
 def pair_find_benchmark(doc_list, permutations, thresholds):
     start = time.perf_counter()
     naive_pairs = find_pairs(doc_list, permutations, thresholds)
     time_basic = time.perf_counter() - start
+    print(f"\nNaive Runtime: {time_basic:.2f}s")
+
     start_lsh = time.perf_counter()
     lsh_pairs = find_pairs_lsh(doc_list, permutations, thresholds)
     time_lsh = time.perf_counter() - start_lsh
+    print(f"LSH Runtime: {time_lsh:.2f}s")
+
+    print()
+    table = []
     for i, t in enumerate(thresholds):
-        naive_count = naive_pairs[i][0]
-        lsh_count = len(lsh_pairs[i])
-        print(f"At Threshold {t} Naive found {naive_count} pairs and LSH found {lsh_count} pairs")
-    print(f"Naive Runtime(s): {time_basic}")
-    print(f"LSH Runtime(s): {time_lsh}")
+        table.append([t, naive_pairs[i][0], len(lsh_pairs[i])])
+    
+    df = pd.DataFrame(table, columns=['theshold', 'naive pairs', 'lsh pairs'])
+    print(df.to_string(index=False))
+
 
 if __name__ == "__main__":
     thresholds = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
-    # print("Running Basic Test")
     # basic_test()
-    # print("Test on a pair of real documents")
     # real_doc_test()
     doc_lists = get_drug_data()
     docs = []
     for doc in doc_lists[0]:
         if type(doc) == type('sr') and len(doc) >= shingle_size:
             docs.append(doc)
-    # print("Running Many Benchmark")
     # benchmark_minhash(docs, 100)
-    # print("Finding pairs")
     # find_pairs_lsh(docs[:100], 10000, thresholds)
     print("Benchmarking pair algos")
     pair_find_benchmark(docs[:100], 1000, thresholds)
