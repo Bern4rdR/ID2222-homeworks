5627d60c29d71f3191dc36b60d4dfb2830d5a9de
Vivienne Curewitz <jaycie.curewitz@gmail.com>
2025-11-19 15:10:16 +0100

    oof

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,21 +1,50 @@
 from import_data import EdgeStream
 import random
+import math
 # global t, s, d0, di# = (0, 0, 0 ,0)
 COUNTER = {
     't': 0,
     's': 0,
     'd0': 0,
     'di':0,
-    'phi': 0
+    'phi': 0,
+    'decrements': 0
 }
 S = []
 M = 6
+triangle_est_at_time_t = []
 
 # edge = (op, (u, v))
 
+def count_neighbors(u, v):
+    if len(S) == 0:
+        return 0
+    u_neigbors = {list(n)[0] if n[0] != u else list(n)[1] for n in S if u in n}
+    v_neighbors = {int(list(n).remove(v)[0]) for n in S if v in n}
+    return len(u_neigbors.intersection(v_neighbors))
+    
+
+def k(): 
+    s = COUNTER['s']
+    di = COUNTER['di']
+    d0 = COUNTER['d0']
+    w = min(M, COUNTER['s']+COUNTER['di']+COUNTER['d0'])
+    return 1 - sum([math.comb(s, j)*math.comb(di+d0, w-j) for j in range(3)]) / math.comb(s+di+d0, w)
+
+
+def p():
+    if len(S) < 3:
+        return 0
+
+    s = COUNTER['s']
+    return (COUNTER['phi']/k()) * ( s*(s-1)*(s-2) )/( len(S)*(len(S)-1)*(len(S)-2) )
+
+
+
 def update_counters(elem):
-    op, _ = elem
-    COUNTER['phi'] += 1 if op == "+" else -1
+    op, (u, v) = elem
+    sn = count_neighbors(u, v)
+    COUNTER['phi'] += sn if op == "+" else -sn
 
 def sample_edge(edge):
     if COUNTER['d0'] + COUNTER['di'] == 0:
@@ -28,7 +57,7 @@ def sample_edge(edge):
         S.append(edge)
         COUNTER['di'] -= 1
     else:
-        COUNTER['d0'] -= 1
+        COUNTER['d0'] -= 1  
         return False
     return True
 
@@ -49,11 +78,15 @@ def main_loop():
             COUNTER['di'] += 1
         else:
             COUNTER['d0'] += 1
-
+        if COUNTER['t'] > 1000:
+            break
         # get ready for next loop
         elem = es.get_next_edge()
 
     print(COUNTER)
+    print(f"K: {k()}")
+    print(f"S: {len(S)}")
+    print(f'triangle count {p()}')
 
 
 if __name__ == "__main__":
