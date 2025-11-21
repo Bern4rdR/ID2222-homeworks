20ddaf2240a1fb3301ef737f05d2ee0b5910d26d
Vivienne Curewitz <jaycie.curewitz@gmail.com>
2025-11-21 17:08:01 +0100

    looking at parallel

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,4 +1,5 @@
 from import_data import EdgeStream
+import time
 import random
 import math
 # global t, s, d0, di# = (0, 0, 0 ,0)
@@ -8,10 +9,12 @@ COUNTER = {
     'd0': 0,
     'di':0,
     'phi': 0,
-    'decrements': 0
+    'decrements': 0,
+    'sample_runtime': 0,
+    'update_runtime': 0,
 }
 S = []
-M = 24
+M = 1000
 triangle_est_at_time_t = []
 
 # edge = (op, (u, v))
@@ -54,19 +57,30 @@ def p():
     return (COUNTER['phi']/k()) * ( s*(s-1)*(s-2) )/( len(S)*(len(S)-1)*(len(S)-2) )
 
 
-
+# this function is the vast majority of the time required to run the algo
+# to parallelize, we have some challenges
+# count neighbors relies on S, but the main loop will update S (but only when it calls count neighbors)
+# we could serialize S, and send it to the thread P;
+# better, S could be kept in shared memory, and P could handle doing the copy
+# so it saves time in the main thread
 def update_counters(elem):
+    start = time.perf_counter()
     op, (u, v) = elem
     sn = count_neighbors(u, v)
     COUNTER['phi'] += sn if op == "+" else -sn
+    COUNTER['update_runtime'] += time.perf_counter() - start
 
 def sample_edge(edge):
+    start = time.perf_counter()
+    u_end = 0
     if COUNTER['d0'] + COUNTER['di'] == 0:
         if len(S) < M:
             S.append(edge)
         elif random.randint(0, COUNTER['t'] - 1) < M:
             rm_edge = S.pop(random.randint(0, len(S) - 1))
+            u_start = time.perf_counter()
             update_counters(('-', rm_edge))
+            u_end = time.perf_counter() - u_start
             S.append(edge)
     elif random.randint(0, COUNTER['di']+COUNTER['d0']) < COUNTER['di']:
         S.append(edge)
@@ -74,6 +88,7 @@ def sample_edge(edge):
     else:
         COUNTER['d0'] -= 1  
         return False
+    COUNTER['sample_runtime'] += time.perf_counter() - start - u_end
     return True
 
 def print_debug():
@@ -86,6 +101,7 @@ def print_debug():
 def main_loop():
     es = EdgeStream()
     elem = es.get_next_edge()
+    total_start = time.perf_counter()
     last_count = 0
     while elem:
         # start main loop here algo 2, section 4.3 triest
@@ -96,21 +112,29 @@ def main_loop():
             if sample_edge((u, v)):
                 update_counters(elem)
         elif (u, v) in S:
+            print("Did remove")
             update_counters(elem) # assuming op is -
             S.remove((u, v))
             COUNTER['di'] += 1
         else:
+            print("Did missing edge remove")
             COUNTER['d0'] += 1
 
+        if COUNTER['t']%5000 == 0:
+            print(f"{COUNTER['t']/50000:0.1f}%", end="\r")
         # if COUNTER['t']%1000 == 0 and last_count != p(): 
         #     print_debug()
         #     last_count = p()
         #     break
-        if COUNTER['t'] > 10000000:
-            break
+        # if COUNTER['t'] > 10000000:
+        #     break
         # get ready for next loop
         elem = es.get_next_edge()
     print_debug()
+    print(f"Total Runtime: {time.perf_counter() - total_start}")
+    print(f"Sample Runtime: {COUNTER['sample_runtime']}")
+    print(f"Update Counter Runtime: {COUNTER['update_runtime']}")
+    print(f"File IO time: {es.runtime}")
     
 
 if __name__ == "__main__":
